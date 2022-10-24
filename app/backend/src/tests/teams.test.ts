d0ef2c2eac377687a2c1f83d757ea7fb4f8a00e8
jpoliveiramateus <joaopedromateus6@gmail.com>
2022-10-24 11:41:42 -0300

    tests: team route test

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,43 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import teams from './expected_results/teams';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teste da rota de teams "/teams"', () => {
+  describe('quando a requisição é feita com sucesso', () => {
+    it('deve retornar um status "200" cotendo todos os times', async () => {
+      const httpResponse = await chai.request(app).get('/teams');
+
+      expect(httpResponse.status).to.be.equal(200);
+      expect(httpResponse.body).to.deep.equal(teams);
+    });
+  });
+});
+
+describe('Teste da rota de teams "/teams/:id"', () => {
+  describe('quando a requisição falha', () => {
+    it('deve retornar um status "404" com a mensagem de erro', async () => {
+      const httpResponse = await chai.request(app).get('/teams/20');
+
+      expect(httpResponse.status).to.be.equal(404);
+      expect(httpResponse.body).to.deep.equal({ message: 'Team not found' });
+    });
+  });
+
+  describe('quando a requisição é feita com sucesso', () => {
+    it('deve retornar um status "200" com o time encontrado', async () => {
+      const httpResponse = await chai.request(app).get('/teams/1');
+
+      expect(httpResponse.status).to.be.equal(200);
+      expect(httpResponse.body).to.deep.equal(teams[0]);
+    });
+  });
+});
+
