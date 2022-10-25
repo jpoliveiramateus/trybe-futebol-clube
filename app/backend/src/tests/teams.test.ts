49ce3000123a90a82c1bd839f8b1577b3b1483b4
jpoliveiramateus <joaopedromateus6@gmail.com>
2022-10-25 10:17:45 -0300

    tests: refactor teams tests with mock

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -6,12 +6,17 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import teams from './expected_results/teams';
 
+import Teams from '../database/models/TeamsModel';
+
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Teste da rota de teams "/teams"', () => {
   describe('quando a requisição é feita com sucesso', () => {
+    beforeEach(() => sinon.stub(Teams, 'findAll').resolves(teams as any));
+    afterEach(sinon.restore);
+
     it('deve retornar um status "200" cotendo todos os times', async () => {
       const httpResponse = await chai.request(app).get('/teams');
 
@@ -23,6 +28,9 @@ describe('Teste da rota de teams "/teams"', () => {
 
 describe('Teste da rota de teams "/teams/:id"', () => {
   describe('quando a requisição falha', () => {
+    beforeEach(() => sinon.stub(Teams, 'findByPk').resolves(null));
+    afterEach(sinon.restore);
+
     it('deve retornar um status "404" com a mensagem de erro', async () => {
       const httpResponse = await chai.request(app).get('/teams/20');
 
@@ -32,6 +40,9 @@ describe('Teste da rota de teams "/teams/:id"', () => {
   });
 
   describe('quando a requisição é feita com sucesso', () => {
+    beforeEach(() => sinon.stub(Teams, 'findByPk').resolves(teams[0] as any));
+    afterEach(sinon.restore);
+
     it('deve retornar um status "200" com o time encontrado', async () => {
       const httpResponse = await chai.request(app).get('/teams/1');
 
